9d6bf1447d70ee43b3363c03ae88baf0a91cdf2c
ray <ray.g@gmx.de>
2015-11-11 00:19:10 +0100

    add random data per second

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-IoT-Frontend
+# IoT-Frontend
+See all the data
+
+## Installation
+
+You need:
+  * node (at least version 4.\*)
+  * mongo (at least version 3.\*)
+installed on your computer.
+
+All set? Good. So on `git clone` this repo.
+Make sure to install all important dependencies.
+`npm install`
+
+OK. You are almost done. We (I) are using gulp for building our files. So run:
+`gulp`
+
+Nice. Now you have all the files where they should be.
+
+Now you can run the server with `node .`
+
+In your browser you can visit now `localhost:3000` to see our nice project in action.
+
+Here is an old Screenshot:
 
 ![](docs/graph.png)
 
--- lib/public/react/Landing.jsx
+++ lib/public/react/Landing.jsx
@@ -4,26 +4,27 @@ var React = require('react'),
 var Landing = React.createClass({
 
   getInitialState: function() {
-    var data = [4, 8, 15, 16, 23, 42];
+    var data = [21, 23, 25, 30, 33, 30];
     return {
-      data: data
+      data: data,
+      graph: null,
+      line: null,
+      xAxis: null,
+      yAxis: null
     };
   },
 
   componentDidMount: function() {
-    // bar chart example
-    //d3.select('.chart')
-      //.selectAll('div')
-        //.data(this.state.data)
-      //.enter().append('div')
-        //.style('width', function(d) { return d * 10 + 'px'; })
-        //.text(function(d) { return d; });
-
-    var m = [80, 80, 80, 80];
+    var m = [80, 80, 80, 80]; // margin
     var w = 1000 - m[1] - m[3];
     var h = 400 - m[0] - m[2];
-    var x = d3.scale.linear().domain([0, this.state.data.length]).range([0, w]);
-    var y = d3.scale.linear().domain([0, 50]).range([h, 0]);
+
+    var x = d3.scale.linear()
+              .domain([0, 60])
+              .range([0, w]);
+    var y = d3.scale.linear()
+              .domain([10, 50])
+              .range([h, 0]);
 
     var line = d3.svg.line()
       .x(function(d,i){
@@ -32,25 +33,58 @@ var Landing = React.createClass({
       .y(function(d){
         return y(d);
       });
+
     var graph = d3.select('#graph').append('svg:svg')
         .attr('width', w + m[1] + m[3])
         .attr('height', h + m[0] + m[2])
       .append('svg:g')
         .attr('transform', 'translate(' + m[3] + ',' + m[0] + ')');
 
-    var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-    graph.append('svg:g')
-      .attr('class', 'x axis')
-      .attr('transform', 'translate(0,' + h + ')')
-      .call(xAxis);
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .tickSize(-h)
+        .tickSubdivide(true);
 
-    var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient('left');
+    graph.append('svg:g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + h + ')')
+            .call(xAxis)
+          .append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('x', 200)
+            .style('text-anchor', 'start')
+            .text('Temperature');
+
+    var yAxisLeft = d3.svg.axis()
+        .scale(y)
+        .ticks(4)
+        .orient('left');
+
     graph.append('svg:g')
       .attr('class', 'y axis')
       .attr('transform', 'translate(-25,0)')
       .call(yAxisLeft);
 
     graph.append('svg:path').attr('d', line(this.state.data));
+
+    this.setState({graph: graph, line: line}, function() {
+      this.update();
+    });
+  },
+
+  setXGraph: function() {
+  },
+
+  update: function() {
+    var that = this;
+    var interval = setInterval(function() {
+      var data = that.state.data;
+      var delta = Math.random() * (2 - -2) + -2;
+      data.push(data[data.length-1] + delta);
+      that.state.graph.append('svg:path').attr('d', that.state.line(data));
+      that.setState({data: data});
+      console.log('timeout');
+    }, 1000);
   },
 
   render: function() {
