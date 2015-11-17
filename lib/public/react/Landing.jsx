d31fc330c6c1e189c984c7b848f8b440e96f5058
ray <ray.g@gmx.de>
2015-11-18 00:23:07 +0100

    use rabbit data for the graph

--- lib/public/react/Landing.jsx
+++ lib/public/react/Landing.jsx
@@ -9,6 +9,7 @@ var Landing = React.createClass({
     var data = [21, 23, 25, 30, 33, 30];
     return {
       data: data,
+      sensorType: '',
       graph: null,
       line: null,
       xAxis: null,
@@ -17,87 +18,82 @@ var Landing = React.createClass({
   },
 
   componentDidMount: function() {
+    var that = this;
     var sock = new SockJS('http://192.168.99.100:15674/stomp');
     var client = Stomp.over(sock);
     client.connect('guest', 'guest', function() {
       console.log('connect');
       var subscription = client
                          .subscribe('/exchange/friss_exch/#', function(msg) {
-        console.log(msg.body);
+        if (!that.state.sensorType) {
+          that.setState({sensorType: msg.headers.sensor_type});
+          var m = [80, 80, 80, 80]; // margin
+          var w = 1000 - m[1] - m[3];
+          var h = 400 - m[0] - m[2];
+
+          var x = d3.scale.linear()
+                    .domain([0, 1000])
+                    .range([0, w]);
+          var y = d3.scale.linear()
+                    .domain([0, 50])
+                    .range([h, 0]);
+
+          var line = d3.svg.line()
+            .x(function(d,i){
+              return x(i);
+            })
+            .y(function(d){
+              return y(d);
+            });
+
+          var graph = d3.select('#graph').append('svg:svg')
+              .attr('width', w + m[1] + m[3])
+              .attr('height', h + m[0] + m[2])
+            .append('svg:g')
+              .attr('transform', 'translate(' + m[3] + ',' + m[0] + ')');
+
+          var xAxis = d3.svg.axis()
+              .scale(x)
+              .tickSize(-h)
+              .tickSubdivide(true);
+
+          graph.append('svg:g')
+                  .attr('class', 'x axis')
+                  .attr('transform', 'translate(0,' + h + ')')
+                  .call(xAxis)
+                .append('text')
+                  .attr('transform', 'rotate(-90)')
+                  .attr('x', 200)
+                  .style('text-anchor', 'start')
+                  .text(that.state.sensorType);
+
+          var yAxisLeft = d3.svg.axis()
+              .scale(y)
+              .ticks(4)
+              .orient('left');
+
+          graph.append('svg:g')
+            .attr('class', 'y axis')
+            .attr('transform', 'translate(-25,0)')
+            .call(yAxisLeft);
+
+          graph.append('svg:path').attr('d', line(that.state.data));
+
+          that.setState({graph: graph, line: line});
+        }
+        var data = that.state.data;
+        var newData = JSON.parse(msg.body).physical_values;
+        newData.forEach(function(d) {
+          data.push(d.value);
+        });
+        that.state.graph.append('svg:path').attr('d', that.state.line(data));
+        that.setState({data: data});
       });
     });
 
-    var m = [80, 80, 80, 80]; // margin
-    var w = 1000 - m[1] - m[3];
-    var h = 400 - m[0] - m[2];
-
-    var x = d3.scale.linear()
-              .domain([0, 10000])
-              .range([0, w]);
-    var y = d3.scale.linear()
-              .domain([0, 150])
-              .range([h, 0]);
-
-    var line = d3.svg.line()
-      .x(function(d,i){
-        return x(i);
-      })
-      .y(function(d){
-        return y(d);
-      });
-
-    var graph = d3.select('#graph').append('svg:svg')
-        .attr('width', w + m[1] + m[3])
-        .attr('height', h + m[0] + m[2])
-      .append('svg:g')
-        .attr('transform', 'translate(' + m[3] + ',' + m[0] + ')');
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .tickSize(-h)
-        .tickSubdivide(true);
-
-    graph.append('svg:g')
-            .attr('class', 'x axis')
-            .attr('transform', 'translate(0,' + h + ')')
-            .call(xAxis)
-          .append('text')
-            .attr('transform', 'rotate(-90)')
-            .attr('x', 200)
-            .style('text-anchor', 'start')
-            .text('Temperature');
-
-    var yAxisLeft = d3.svg.axis()
-        .scale(y)
-        .ticks(4)
-        .orient('left');
-
-    graph.append('svg:g')
-      .attr('class', 'y axis')
-      .attr('transform', 'translate(-25,0)')
-      .call(yAxisLeft);
-
-    graph.append('svg:path').attr('d', line(this.state.data));
-
-    this.setState({graph: graph, line: line}, function() {
-      this.update();
-    });
   },
 
   setXGraph: function() {
-  },
-
-  update: function() {
-    var that = this;
-    var interval = setInterval(function() {
-      var data = that.state.data;
-      for (var i = 0; i < 20; i++) {
-        var delta = Math.random() * (2 - -2) + -2;
-        data.push(data[data.length-1] + delta);
-      }
-      that.state.graph.append('svg:path').attr('d', that.state.line(data));
-      that.setState({data: data});
-    }, 1000);
   },
 
   render: function() {
